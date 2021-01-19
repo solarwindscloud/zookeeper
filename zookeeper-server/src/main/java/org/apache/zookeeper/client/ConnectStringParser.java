ed965a4e1580294e3d28a262712e2cfe8039d022
Daniel Chemko <daniel.chemko@solarwinds.com>
2021-01-18 16:03:07 -0800

    SP-2583: Fix name resolver

--- zookeeper-server/src/main/java/org/apache/zookeeper/client/ConnectStringParser.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/client/ConnectStringParser.java
@@ -1,51 +1,31 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.apache.zookeeper.client;
 
-import static org.apache.zookeeper.common.StringUtils.split;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
+
 import org.apache.zookeeper.common.NetUtils;
 import org.apache.zookeeper.common.PathUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.apache.zookeeper.common.StringUtils.split;
 
 /**
- * A parser for ZooKeeper Client connect strings.
- *
- * This class is not meant to be seen or used outside of ZooKeeper itself.
- *
- * The chrootPath member should be replaced by a Path object in issue
- * ZOOKEEPER-849.
- *
- * @see org.apache.zookeeper.ZooKeeper
+ * Workaround for Zookeeper defect in JDK-14+
  */
 public final class ConnectStringParser {
-
     private static final int DEFAULT_PORT = 2181;
+    private static Logger log = LoggerFactory.getLogger(ConnectStringParser.class);
 
     private final String chrootPath;
 
     private final ArrayList<InetSocketAddress> serverAddresses = new ArrayList<InetSocketAddress>();
 
     /**
-     * Parse host and port by spliting client connectString
-     * with support for IPv6 literals
+     *
      * @throws IllegalArgumentException
      *             for an invalid chroot path.
      */
@@ -96,5 +76,4 @@ public final class ConnectStringParser {
     public ArrayList<InetSocketAddress> getServerAddresses() {
         return serverAddresses;
     }
-
 }
